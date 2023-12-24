5260f8828b356fc9fb2d3eff527d617acd10ddd5
kaustubh <kaustubhkj@gmail.com>
2023-12-24 20:59:55 +0530

    monkey lang test input

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -51,11 +51,27 @@ func (l *Lexer) NextToken() token.Token {
 		return newToken(token.RBRACE, l.ch)
 	case 0:
 		return token.Token{Type: token.EOF, Literal: ""}
+	default:
+		if isLetter(l.ch) {
+			return token.Token{Literal: l.readIdentifier()}
+		}
 	}
 
-	return newToken(token.ILLEGAL, 0)
+	return newToken(token.ILLEGAL, l.ch)
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
 }
